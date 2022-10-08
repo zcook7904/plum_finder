3da1ed5173d57dc796484e894c33c16bfe88128e
Zaven <zcook7904@gmail.com>
2022-10-07 17:19:21 -0700

    Created dataclass for user address

--- src/SF_plum_finder/plum_finder.py
+++ src/SF_plum_finder/plum_finder.py
@@ -4,6 +4,7 @@ and returns the nearest plum tree to that location"""
 import json
 import configparser
 import os
+from dataclasses import dataclass
 from time import time
 import argparse
 import csv
@@ -13,6 +14,57 @@ from warnings import warn
 from SF_plum_finder.API_handling import get_geolocation, create_client
 from init_config import init_config_file
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+
+@dataclass
+class Address:
+    """Dataclass to represent address. str returns the street address as a string and
+    len the number of elements in the street address."""
+    street_address: str
+    city: str = 'San Francisco, CA'
+    latitude: float = None
+    longitude: float = None
+
+    def __str__(self):
+        return self.street_address
+
+    def __len__(self):
+        return len(self.get_address_as_list())
+
+    def get_address_as_list(self):
+        """Converts the inputted address to a list if it is in string form. Returns the address as a list"""
+        address_list = self.street_address.split(' ')
+        return address_list
+
+    def get_number(self):
+        return int(self.get_address_as_list()[0])
+
+    def get_street_name(self):
+        return ' '.join(self.get_address_as_list()[1:])
+
+    def get_full_address(self):
+        return f'{self.street_address}, {self.city}'
+
+    def set_number(self, new_number: int):
+        street_name = self.get_street_name()
+        self.street_address = f'{new_number} {street_name}'
+
+    def set_street_name(self, new_name: str):
+        street_number = self.get_number()
+        self.street_address = f'{street_number} {new_name}'
+
+    def set_geolocation(self, gmaps):
+        """Get the geolocation of the address"""
+        try:
+            latitude, longitude = get_geolocation(gmaps, self.get_full_address())
+            self.latitude = latitude
+            self.longitude = longitude
+        except ValueError:
+            # API used incorrectly
+            return 494
+        except RuntimeError:
+            # timed out
+            return 592
 
 
 def load_config():
@@ -65,6 +117,7 @@ def create_performance_log():
         with open('performance_log.csv', 'x', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(header)
+            return True
 
     except FileExistsError:
         print('Performance log already exists')
@@ -100,41 +153,45 @@ def timer_func(func):
 
 
 def _get_cli_args():
-    """Argument parser for CLI"""
+    """Argument parser for CLI. Returns address args as a string"""
     parser = argparse.ArgumentParser(description='Get the location of the closest plum tree!')
     parser.add_argument('-n', type=int, default=_n,
                         help='the number of potential closest trees to be sent to googlemaps. Max allowed: 25')
     parser.add_argument('address', nargs='+',
                         help='a street address located in San Francisco, CA')
     args = parser.parse_args()
-    return args.address, args.n
+    address = ' '.join(args.address)
+    return address, args.n
 
 
-def check_address_arg_length(input_address) -> bool:
+def check_address_arg_length(address: Address) -> bool:
     """ Returns false if the input address doesn't have at least 3 components"""
-    arg_length = len(input_address)
-    # make sure input is correct length
+    arg_length = len(address)
+    # make sure input is correct length, max 10 is arb...
     if arg_length < 3 or arg_length > 10:
         # Address should contain {number} {street_name} {street_suffix} at minimum
         return False
     return True
 
 
-def check_street_number(street_number) -> bool:
+def check_street_number(address: Address) -> bool:
     """ Returns True if the street number is valid"""
     # ensure first input is an integer
     try:
-        int(street_number)
+        # should throw value error if street number cannot be returned
+        address.get_number()
         return True
     except ValueError:
         return False
 
 
-def format_street_name(street_name: str) -> str:
+def format_street_name(address: Address) -> Address:
     """adds '0' to street name if street name is numeric, <10, and doesn't contain '0' (ex 9th st -> 09th st)"""
+    street_name = address.get_street_name()
     if street_name[0].isnumeric() and not street_name[1].isnumeric():
-        street_name = '0' + street_name
-    return street_name
+        new_street_name = '0' + street_name
+        address.set_street_name(new_street_name)
+    return address
 
 
 def load_SF_streets(street_name_path: str = os.path.join(data_dir, 'Street_Names.json')) -> list | None:
@@ -150,39 +207,25 @@ def load_SF_streets(street_name_path: str = os.path.join(data_dir, 'Street_Names
         return None
 
 
-def check_if_street_in_SF(acceptable_street_names, street: str) -> bool:
+def check_if_street_in_SF(acceptable_street_names, address: Address) -> bool:
     """Returns True if the street name is in the SF street name list"""
-    return street.casefold() in (name.casefold() for name in acceptable_street_names)
+
+    return address.get_street_name().casefold() in (name.casefold() for name in acceptable_street_names)
 
 
-def _convert_address_to_list(input_address: str | list) -> list:
-    """Converts the inputted address to a list if it is in string form. Returns the address as a list"""
-    if type(input_address) == str:
-        input_address = input_address.split(' ')
-    return input_address
-
-
-def process_address(input_address: str | list, add_city: bool = True) -> str | int:
+def process_address(address: Address, add_city: bool = True) -> Address | int:
     """ verify the user has given a real address and process the address into something manageable by
     the Google Maps api.
     Accepts list in format [number, street_name, stree_type] and returns string of full postal address"""
-    input_address = _convert_address_to_list(input_address)
 
-    if not check_address_arg_length(input_address):
+    if not check_address_arg_length(address):
         return 490
 
-    street_number = input_address[0]
-    if not check_street_number(street_number):
+    if not check_street_number(address):
         return 491
 
     # clean up street name
-    street_name = input_address[1:-1]
-    street_name = ' '.join(street_name)
-    street_name = format_street_name(street_name)
-
-    # join name and type make street
-    street_type = input_address[-1]
-    street = ' '.join([street_name, street_type])
+    address = format_street_name(address)
 
     # load json containing all street names in SF
     acceptable_streets = load_SF_streets()
@@ -190,17 +233,9 @@ def process_address(input_address: str | list, add_city: bool = True) -> str | i
         # file unable to load
         return 492
 
-    if not check_if_street_in_SF(acceptable_streets, street):
+    if not check_if_street_in_SF(acceptable_streets, address):
         # street
         return 493
-
-    # join everything together and return
-    address = street_number + ' ' + street
-
-    # adds city suffix by default
-    if add_city:
-        city_address = 'SAN FRANCISCO, CA'
-        address = address + ', ' + city_address
 
     return address
 
@@ -212,32 +247,39 @@ def get_geocode_from_db(user_address):
 
 
 @timer_func
-def get_user_geolocation(gmaps, processed_address: str):
+def load_tree_data(path: str):
+    """Loads the Plum_Street_Tree_List.csv file into a pandas dataframe and returns it"""
     try:
-        latitude, longitude = get_geolocation(gmaps, processed_address)
-        return latitude, longitude
-    except ValueError:
-        # API used incorrectly
-        return 494
-    except RuntimeError:
-        # timed out
-        return 592
+        tree_data = pd.read_csv(path).set_index(['TreeID'])
+    except FileNotFoundError:
+        # tree data not found
+        return 593
+    
+    if tree_data.empty:
+        # tree data loaded incorrectly
+        return 594
+    
+    # only need one tree per address
+    tree_data.drop_duplicates('qAddress', inplace=True)
+    
+    return tree_data
+            
 
-
-def approximate_distance(data: pd.DataFrame, latitude: float, longitude: float) -> pd.DataFrame:
+def approximate_distance(data: pd.DataFrame, origin_address: Address) -> pd.DataFrame:
     """Adds the geometric distance from a given latitude and longitude to the street_tree_list data frame."""
 
     # create numpy arrays for latitudes, longitude, and then compute distances from given latitude and longitude
     latitudes = data.loc[:, 'Latitude'].values
     longitudes = data.loc[:, 'Longitude'].values
-    geometric_distances = np.sqrt((latitudes - latitude) ** 2 + (longitudes - longitude) ** 2)
+    geometric_distances = np.sqrt((latitudes - origin_address.latitude) ** 2
+                                  + (longitudes - origin_address.longitude) ** 2)
 
     data.insert(7, 'geometric_distance', geometric_distances)
 
     return data
 
 
-def verify_closest(client, origin_address, destinations: list, mode: str = 'walking',
+def verify_closest(gmaps, origin_address: Address, destinations: list, mode: str = 'walking',
                    sort: bool = True, distance_diff: bool = False) -> list | tuple:
     """verify which tree is actually closest walking-wise from Google Maps api call.
     Accepts Google Maps API key, a single origin, and a list of destinations. Origin and destinations can be in form
@@ -246,15 +288,8 @@ def verify_closest(client, origin_address, destinations: list, mode: str = 'walk
     If sort is true, returned list will be sorted by distance in with the smallest distance first.
     Returns list of dictionary in form {'address': {address}, 'distance': {distance in m}}'"""
 
-    # ensure only one origin is passed to gmaps
-    if type(origin_address) == list:
-        origin_address = list[0]
-        warn('This function typically only accepts one origin. '
-             'Response will only be distances from first origin in list')
-
     # request google and get response
-    gmaps = client
-    response = gmaps.distance_matrix(origin_address, destinations, mode=mode)
+    response = gmaps.distance_matrix(origin_address.get_full_address(), destinations, mode=mode)
 
     # create list of distances
     distances = []
@@ -270,27 +305,15 @@ def verify_closest(client, origin_address, destinations: list, mode: str = 'walk
 
         distances.sort(key=by_distance)
 
+    # the difference between the closest tree geometrically and the
+    # geographically closest in geographical distance
     distance_difference = original_shortest_distance - distances[0]['distance']
 
-    # returns the difference between the closest tree geometrically and the
-    # geographically closest in geographical distance
+    # if desired, returns a tuple containing the distances and the distance difference
     if distance_diff:
         return distances, distance_difference
 
     return distances
-
-
-def return_tree(closest_tree, input_address):
-    # return the closest tree
-    address = closest_tree['qAddress'][0]
-    species = closest_tree['qSpecies'][0]
-    distance = closest_tree['street_distance'][0]
-
-    response = """Closest tree to {}:
-Species: {}
-Address: {}
-Distance: {}m""".format(input_address, species, address, distance)
-    return response
 
 
 def open_last_response(path):
@@ -302,7 +325,7 @@ def open_last_response(path):
         print("Couldn't find file")
 
 
-def find_closest_plum(user_address: str | list, key: str, n: int):
+def find_closest_plum(input_address: str, key: str, n: int):
 
     # only allow up to 25 trees
     if n > 25:
@@ -311,11 +334,13 @@ def find_closest_plum(user_address: str | list, key: str, n: int):
     if performance_log:
         start_time = time()
 
-    processed_address = process_address(user_address)
+    user_address = Address(input_address)
+
+    user_address = process_address(user_address)
 
     # if process_address raises an error code, return the code
-    if type(processed_address) == int:
-        return processed_address
+    if type(user_address) == int:
+        return user_address
 
     try:
         gmaps = create_client(key)
@@ -323,51 +348,33 @@ def find_closest_plum(user_address: str | list, key: str, n: int):
         # invalid API key used
         return 591
 
-    # user's geographic location
-    geocode = get_user_geolocation(gmaps, processed_address)
+    # sets user's geographic location
+    try:
+        user_address.set_geolocation(gmaps)
 
-    # return error if occurs
-    if isinstance(geocode, int):
-        return geocode
-
-    latitude, longitude = geocode
-
-    # response = open_last_response('test.json')
-    # latitude = float(response['geometry']['location']['lat'])
-    # longitude = float(response['geometry']['location']['lng'])
+    except ValueError:
+        # API used incorrectly
+        return 494
+    except RuntimeError:
+        # timed out
+        return 592
 
     # load tree data
+    # may make this async in future
     tree_data_path = os.path.join(data_dir, 'Plum_Street_Tree_List.csv')
-
-    # TODO load data while getting geocode/processing address? call at top and retrieve here
-    # also move this to single function that ends...
-    @timer_func
-    def load_data(path):
-        try:
-            tree_data = pd.read_csv(path).set_index(['TreeID'])
-            return tree_data
-        except FileNotFoundError:
-            # tree data not found
-            return 593
-
-    if type(data) == int:
-        return data
-
-    if data.empty:
-        # tree data loaded incorrectly
-        return 594
+    tree_data = load_tree_data(tree_data_path)
+    
+    if type(tree_data) == int:
+        return tree_data
 
     # add distance from inputted address to data frame
-    data.drop_duplicates('qAddress', inplace=True)
-
-    # ...here
-    data = approximate_distance(data, latitude, longitude)
+    tree_data = approximate_distance(tree_data, user_address)
 
     # get the n shortest distances and create a dict containing the addresses to send to gmaps
-    approx_closest_trees = data.nsmallest(n, 'geometric_distance')
+    approx_closest_trees = tree_data.nsmallest(n, 'geometric_distance')
 
     destinations = approx_closest_trees.qAddress.to_list()
-    city_suffix = ', San Francisco, CA'
+    city_suffix = user_address.city
     len_city_suffix = len(city_suffix)
 
     for i, address in enumerate(destinations):
@@ -375,9 +382,9 @@ def find_closest_plum(user_address: str | list, key: str, n: int):
         destinations[i] = full_address
 
     # query gmaps for sorted distance matrix of possible closest trees
-    # distance difference ives difference between approximate closest and actual closest
+    # distance difference gives difference between approximate closest and actual closest
     try:
-        distances, distances_difference = verify_closest(gmaps, processed_address, destinations, distance_diff=True)
+        distances, distances_difference = verify_closest(gmaps, user_address, destinations, distance_diff=True)
     except ValueError:
         # API used incorrectly
         return 494
@@ -386,20 +393,18 @@ def find_closest_plum(user_address: str | list, key: str, n: int):
         return 592
 
     closest_address = distances[0]['address'][0:-len_city_suffix]
-    closest_tree = data.loc[data.qAddress == closest_address]
+    closest_tree = tree_data.loc[tree_data.qAddress == closest_address]
     response = closest_tree.to_dict('list')
     response['street_distance'] = [distances[0]['distance']]
 
     # logging data for performance and usage monitoring
     if performance_log:
         total_time = time() - start_time
-        approx_closest_tree = approx_closest_trees.iloc[0, :]
+        approx_closest_tree = approx_closest_trees.iloc[0]
         closest_tree = closest_tree.iloc[0]
-        if isinstance(user_address, list):
-            user_address = ' '.join(user_address)
 
         # creates row and appends it to performance_log.csv
-        log_row = [user_address, latitude, longitude,
+        log_row = [user_address.street_address, user_address.latitude, user_address.longitude,
                    approx_closest_tree.name, approx_closest_tree.loc['Latitude'], approx_closest_tree.loc['Longitude'],
                    closest_tree.name, closest_tree.loc['Latitude'], closest_tree.loc['Longitude'],
                    distances_difference, use_SQL, total_time]
@@ -417,15 +422,26 @@ def command_line_runner(text_input=None):
     else:
         CLI_address, CLI_n = _get_cli_args()
 
-    CLI_address = _convert_address_to_list(CLI_address)
     closest_plum = find_closest_plum(CLI_address, config['Keys']['GoogleMaps'], n=CLI_n)
 
     if type(closest_plum) == int:
         print(error_dict[closest_plum])
     else:
-        address = ' '.join(CLI_address)
-        response = return_tree(closest_plum, address)
+        response = return_tree(closest_plum, CLI_address)
         print(response)
+
+
+def return_tree(closest_tree, input_address: str):
+    # return the closest tree
+    address = closest_tree['qAddress'][0]
+    species = closest_tree['qSpecies'][0]
+    distance = closest_tree['street_distance'][0]
+
+    response = """Closest tree to {}:
+Species: {}
+Address: {}
+Distance: {}m""".format(input_address, species, address, distance)
+    return response
 
 
 if __name__ == '__main__':
